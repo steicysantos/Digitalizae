bc2d1059e105eac0ce9f5fbc80dd65ad1d3d8099
Nathan <nathan.ansayc@gmail.com>
2023-05-08 19:32:11 -0300

    Tela histórico

--- front-digitalizae/src/app/historico-processos/historico-processos.component.ts
+++ front-digitalizae/src/app/historico-processos/historico-processos.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { Processo } from '../Classes';
+import { CadastroCandidatoComponent } from '../cadastro-candidato/cadastro-candidato.component';
 @Component({
   selector: 'app-historico-processos',
   templateUrl: './historico-processos.component.html',
   styleUrls: ['./historico-processos.component.css']
 })
 export class HistoricoProcessosComponent {
+  processo : Processo = {
+    nome:"Desenvolvimento de sistemas",
+    DataFim: Date.now().toString(),
+    DataInicio: Date.now().toString(),
+    ativo: true,
+    qtdeMax: 500
+  }
 
+  isVisible : boolean = false
+
+  processos: Array<Processo> = []
+  ngOnInit(): void {
+    this.processos.push(this.processo)
+  }
+
+  Visual(){
+    if(this.isVisible){
+      this.isVisible = false
+      console.log(this.isVisible)
+      return
+    }
+      this.isVisible = false
+      console.log(this.isVisible)
+  }
 }
+
--- front-digitalizae/src/app/tabela-processo-seletivo/tabela-processo-seletivo.component.ts
+++ front-digitalizae/src/app/tabela-processo-seletivo/tabela-processo-seletivo.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { Processo } from '../Classes';
 @Component({
   selector: 'app-tabela-processo-seletivo',
   templateUrl: './tabela-processo-seletivo.component.html',
   styleUrls: ['./tabela-processo-seletivo.component.css']
 })
 export class TabelaProcessoSeletivoComponent {
+  
 
 }
