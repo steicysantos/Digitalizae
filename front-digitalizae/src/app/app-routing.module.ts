564f02adab7a6adeae83d65144f6d74ce8a1d04f
steicy <steicymsantos@gmail.com>
2023-05-08 14:50:00 -0300

    iniciando o projeto para finalizar daqui 3 dias

--- front-digitalizae/src/app/app-routing.module.ts
+++ front-digitalizae/src/app/app-routing.module.ts
@@ -3,12 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CadastroCandidatoComponent } from './cadastro-candidato/cadastro-candidato.component';
 import { CadastroProcessoSeletivoComponent } from './cadastro-processo-seletivo/cadastro-processo-seletivo.component';
+import { HistoricoProcessosComponent } from './historico-processos/historico-processos.component';
+import { VisualizaFaseComponent } from './visualiza-fase/visualiza-fase.component';
+import { TabelaProcessoSeletivoComponent } from './tabela-processo-seletivo/tabela-processo-seletivo.component';
+import { CadastroNovasFasesComponent } from './cadastro-novas-fases/cadastro-novas-fases.component';
+import { PerfilCandidatoComponent } from './perfil-candidato/perfil-candidato.component';
+import { LocalProvaComponent } from './local-prova/local-prova.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'cadastro-candidato', component: CadastroCandidatoComponent},
   { path: 'cadastro-processo-seletivo', component: CadastroProcessoSeletivoComponent},
-
+  { path: 'historico-processos', component: HistoricoProcessosComponent},
+  { path: 'visualiza-fase', component:VisualizaFaseComponent},
+  { path: 'tabela-processo-seletivo', component:TabelaProcessoSeletivoComponent},
+  { path: 'cadastro-novas-fases', component:CadastroNovasFasesComponent},
+  { path: 'perfil-candidato', component:PerfilCandidatoComponent},
+  { path: 'local-prova', component:LocalProvaComponent}
 ];
 
 @NgModule({
