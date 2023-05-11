41488968e528ae7dafb308fd5f28e8ae168263dd
Nathan <nathan.ansayc@gmail.com>
2023-05-11 16:37:32 -0300

    Alterações nates

--- front-digitalizae/src/app/Classes.ts
+++ front-digitalizae/src/app/Classes.ts
@@ -1,3 +1,5 @@
+import { subscriptionLogsToBeFn } from "rxjs/internal/testing/TestScheduler";
+
 export interface Processo{
     nome : string,
     DataInicio: string;
@@ -5,3 +7,25 @@ export interface Processo{
     ativo : boolean,
     qtdeMax : number
 }
+
+export interface Candidato{
+    id : number,
+    nome: string,
+    dataNascimento: Date,
+    telefone : string,
+    email : string,
+    nomeProcesso : string,
+    curriculo : string,
+    login: string,
+    senha: string,
+    ativo : boolean,
+}
+
+export interface Fase {
+    id: number,
+    idProcessoSeletivo: number,
+    tipo : true,
+    descricao: string,
+    local: string,
+}
+
--- front-digitalizae/src/app/perfil-candidato/perfil-candidato.component.ts
+++ front-digitalizae/src/app/perfil-candidato/perfil-candidato.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-
+import axios from 'axios';
+import { Candidato } from '../Classes';
 @Component({
   selector: 'app-perfil-candidato',
   templateUrl: './perfil-candidato.component.html',
   styleUrls: ['./perfil-candidato.component.css']
 })
 export class PerfilCandidatoComponent {
+
+  candidato : Candidato = {
+    id : 1,
+    nome: "string",
+    dataNascimento: new Date("1900-01-01"),
+    telefone : "string",
+    email : "string",
+    curriculo : "string",
+    nomeProcesso : "string",
+    login: "string",
+    senha: "string",
+    ativo : false,
+  }
+  
+  ngOnInit(): void {
+    var id = "4"
+    var config = {
+      method: 'get',
+      url: 'http://localhost:5164/CandidatoxProcesso/GetWithInfo/'+id,
+      headers: { },
+    };
+
+    var instance = this
+    axios(config)
+      .then(function (response) {
+        instance.candidato = response.data
+        console.log(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  }
 
 }
