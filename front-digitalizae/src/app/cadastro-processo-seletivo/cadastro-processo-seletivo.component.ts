3bd48a49f35593bff75d700dcf0e295361b783a7
steicy <steicymsantos@gmail.com>
2023-05-15 09:21:36 -0300

    hj eh o dia da entrega

--- front-digitalizae/src/app/cadastro-processo-seletivo/cadastro-processo-seletivo.component.ts
+++ front-digitalizae/src/app/cadastro-processo-seletivo/cadastro-processo-seletivo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from "axios";
 
 @Component({
   selector: 'app-cadastro-processo-seletivo',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro-processo-seletivo.component.css']
 })
 export class CadastroProcessoSeletivoComponent {
+  constructor() { }
 
+  ngOnInit(): void {
+
+  }
+
+  ProcessoRegister() {
+    let curso = (document.getElementById("curso") as HTMLInputElement).value;
+    let inicio = (document.getElementById("inicio") as HTMLInputElement).value;
+    let fim = (document.getElementById("fim") as HTMLInputElement).value;
+    let numcandidatos = (document.getElementById("numcandidatos") as HTMLInputElement).value;
+
+    var data = JSON.stringify({
+      curso: curso,
+      inicio: inicio,
+      fim: fim,
+      numcandidatos: numcandidatos
+    });
+
+    var config = {
+      method: 'post',
+      url: '',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: data
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+        alert("Cadastro concluído!");
+      })
+      .catch(function (error) {
+        alert(error);
+        console.log(error);
+      });
+  }
 }
